1b7a6174044bdcdcfaf423a78afceeb795ff4f56
lonewolfyx <877085893@qq.com>
2025-09-10 16:57:31 +0800

    chore(team): update member list

--- app/constant/members.ts
+++ app/constant/members.ts
@@ -2,7 +2,7 @@ export const members = [
     // 'Alfred-Skyblue',
     'pkc918',
     'yuguaa',
-    'ArthurDarkstone',
+    'ArthurDarkstone', // 高手哥
     'lonewolfyx',
     'broBinChen',
     'ilyaliao',
@@ -14,4 +14,10 @@ export const members = [
     'TAYUN',
     'NoiseFan',
     'jsy-0526', // 阳哥
+    'wan-kong',
+    'penghangtao',
+    'lbineau',
+    'LarryZhu-dev',
+    'Bao0630',
+    'heyrian', // 日安小姐姐
 ]

23888ed82050f1de75dca15a973341bc630f7589
noise <noisefan@163.com>
2025-10-06 01:22:37 +0800

    typo: format style

--- app/components/Github/GithubDashboard.vue
+++ app/components/Github/GithubDashboard.vue
@@ -1,73 +1,78 @@
 <template>
-  <div class="relative space-y-6">
-    <GithubUserInfo :user-info="data['userInfo']" :aliasName="aliasName" />
-
-    <div class="grid lg:grid-cols-3 gap-5 lg:gap-7.5 items-stretch">
-      <div class="lg:col-span-1 space-y-8">
-        <GithubMostUsedLanguages
-          v-if="data['languages']"
-          :languages="data['languages']"
+    <div class="relative space-y-6">
+        <GithubUserInfo
+            :user-info="data['userInfo']"
+            :alias-name="aliasName"
         />
 
-        <GitHubContributorsHeatmap
-          :contributions="data['contributions']['contributions']"
-          :end-date="`${dayjs().year()}-12-31`"
-          :total="data['contributions']['total']"
-        />
+        <div class="grid lg:grid-cols-3 gap-5 lg:gap-7.5 items-stretch">
+            <div class="lg:col-span-1 space-y-8">
+                <GithubMostUsedLanguages
+                    v-if="data['languages']"
+                    :languages="data['languages']"
+                />
 
-        <GithubOrganizations
-          v-if="data.organizations.nodes"
-          :organizations="data.organizations.nodes"
-        />
-      </div>
-      <div class="lg:col-span-2">
-        <GithubPullRequest :pull-requests="data.pullRequest" />
-      </div>
+                <GitHubContributorsHeatmap
+                    :contributions="data['contributions']['contributions']"
+                    :end-date="`${dayjs().year()}-12-31`"
+                    :total="data['contributions']['total']"
+                />
+
+                <GithubOrganizations
+                    v-if="data.organizations.nodes"
+                    :organizations="data.organizations.nodes"
+                />
+            </div>
+            <div class="lg:col-span-2">
+                <GithubPullRequest :pull-requests="data.pullRequest" />
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts" setup>
-import type { IGithubUserData } from "~/types"
-import GithubUserInfo from "~/components/Github/GithubUserInfo.vue"
-import GitHubContributorsHeatmap from "~/components/Github/GitHubContributorsHeatmap.vue"
-import GithubOrganizations from "~/components/Github/GithubOrganizations.vue"
-import GithubPullRequest from "~/components/Github/GithubPullRequest.vue"
-import dayjs from "dayjs"
+import type { IGithubUserData } from '~/types'
+import GithubUserInfo from '~/components/Github/GithubUserInfo.vue'
+import GitHubContributorsHeatmap from '~/components/Github/GitHubContributorsHeatmap.vue'
+import GithubOrganizations from '~/components/Github/GithubOrganizations.vue'
+import GithubPullRequest from '~/components/Github/GithubPullRequest.vue'
+import dayjs from 'dayjs'
 
 defineOptions({
-  name: "GithubDashboard",
+    name: 'GithubDashboard',
 })
 
 const props = defineProps<{
-  data: IGithubUserData
-  aliasName?: string
+    data: IGithubUserData
+    aliasName?: string
 }>()
 
 const contributors = [
-  { date: "2025-07-01", count: 0 },
-  { date: "2025-07-02", count: 2 },
-  { date: "2025-07-03", count: 5 },
-  { date: "2025-07-04", count: 8 },
-  { date: "2025-07-05", count: 12 },
-  { date: "2025-07-06", count: 0 },
-  { date: "2025-07-07", count: 0 },
-  { date: "2025-07-08", count: 0 },
-  { date: "2025-07-09", count: 1 },
-  { date: "2025-07-10", count: 4 },
-  { date: "2025-07-11", count: 7 },
-  { date: "2025-07-12", count: 10 },
-  { date: "2025-07-13", count: 0 },
-  { date: "2025-07-14", count: 0 },
-  { date: "2025-07-15", count: 0 },
-  { date: "2025-07-16", count: 1 },
-  { date: "2025-07-17", count: 4 },
-  { date: "2025-07-18", count: 7 },
-  { date: "2025-07-19", count: 2222 },
-  { date: "2025-07-20", count: 0 },
-  { date: "2025-07-21", count: 0 },
-  { date: "2025-09-09", count: 456789 },
+    { date: '2025-07-01', count: 0 },
+    { date: '2025-07-02', count: 2 },
+    { date: '2025-07-03', count: 5 },
+    { date: '2025-07-04', count: 8 },
+    { date: '2025-07-05', count: 12 },
+    { date: '2025-07-06', count: 0 },
+    { date: '2025-07-07', count: 0 },
+    { date: '2025-07-08', count: 0 },
+    { date: '2025-07-09', count: 1 },
+    { date: '2025-07-10', count: 4 },
+    { date: '2025-07-11', count: 7 },
+    { date: '2025-07-12', count: 10 },
+    { date: '2025-07-13', count: 0 },
+    { date: '2025-07-14', count: 0 },
+    { date: '2025-07-15', count: 0 },
+    { date: '2025-07-16', count: 1 },
+    { date: '2025-07-17', count: 4 },
+    { date: '2025-07-18', count: 7 },
+    { date: '2025-07-19', count: 2222 },
+    { date: '2025-07-20', count: 0 },
+    { date: '2025-07-21', count: 0 },
+    { date: '2025-09-09', count: 456789 },
 ]
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
--- app/constant/members.ts
+++ app/constant/members.ts
@@ -1,28 +1,28 @@
 type membersType = {
-  username: string
-  aliasName?: string
+    username: string
+    aliasName?: string
 }
 export const members: membersType[] = [
-  { username: "pkc918", aliasName: "肉丝哥" }, // 肉丝哥
-  { username: "kovsu" }, // antdv 团队成员
-  { username: "ilyaliao" }, // vueuse 团队成员
-  { username: "Bao0630" }, // ant-design-x-vue 团队成员
-  { username: "yuguaa", aliasName: "菠萝吹雪" }, // Element-Plus-X
-  { username: "lonewolfyx" },
-  { username: "ArthurDarkstone", aliasName: "高手哥" }, // 高手哥
-  { username: "broBinChen", aliasName: "木木哥" }, // 木木哥
-  { username: "ffgenius" }, // 粉红色的回忆
-  { username: "heyrian", aliasName: "日安" }, // 日安小姐姐
-  { username: "LarryZhu-dev" },
-  { username: "doyuli" },
-  { username: "keeplearning66" },
-  { username: "qianYuanJ" },
-  { username: "snowbitx" },
-  { username: "TAYUN" },
-  { username: "NoiseFan", aliasName: "澎澎同学" },
-  { username: "jsy-0526", aliasName: "阳哥" }, // 阳哥
-  { username: "wan-kong" },
-  { username: "penghangtao" },
-  { username: "lbineau" },
-  { username: "Chang-Chen" },
+    { username: 'pkc918', aliasName: '肉丝哥' }, // 肉丝哥
+    { username: 'kovsu' }, // antdv 团队成员
+    { username: 'ilyaliao' }, // vueuse 团队成员
+    { username: 'Bao0630' }, // ant-design-x-vue 团队成员
+    { username: 'yuguaa', aliasName: '菠萝吹雪' }, // Element-Plus-X
+    { username: 'lonewolfyx' },
+    { username: 'ArthurDarkstone', aliasName: '高手哥' }, // 高手哥
+    { username: 'broBinChen', aliasName: '木木哥' }, // 木木哥
+    { username: 'ffgenius' }, // 粉红色的回忆
+    { username: 'heyrian', aliasName: '日安' }, // 日安小姐姐
+    { username: 'LarryZhu-dev' },
+    { username: 'doyuli' },
+    { username: 'keeplearning66' },
+    { username: 'qianYuanJ' },
+    { username: 'snowbitx' },
+    { username: 'TAYUN' },
+    { username: 'NoiseFan', aliasName: '澎澎同学' },
+    { username: 'jsy-0526', aliasName: '阳哥' }, // 阳哥
+    { username: 'wan-kong' },
+    { username: 'penghangtao' },
+    { username: 'lbineau' },
+    { username: 'Chang-Chen' },
 ]

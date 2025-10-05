25148702baf20b0fa12f2fd8a6df03d30895bef6
noise <noisefan@163.com>
2025-10-06 01:10:07 +0800

    feat(GithubUserInfo): add alias name

--- app/components/Github/GithubUserInfo.vue
+++ app/components/Github/GithubUserInfo.vue
@@ -1,68 +1,72 @@
 <template>
-    <div
-        class="bg-center bg-cover bg-no-repeat bg-[url('/hero.png')] mb-5"
-    >
-        <div class="px-4">
-            <div class="flex flex-col items-center gap-2 lg:gap-3.5 py-4 lg:pt-5 lg:pb-10">
-                <NuxtLink
-                    :title="`${userInfo.login}·${userInfo.name}`"
-                    :to="userInfo.url"
-                    target="_blank"
-                >
-                    <img
-                        :alt="`${userInfo.login}·${userInfo.name}`"
-                        :src="userInfo.avatarUrl"
-                        class="rounded-full size-28 shadow-[0_0_5px_1px_theme(color.green.500)] shrink-0"
-                    >
-                </NuxtLink>
-                <div class="flex items-center gap-1.5">
-                    <NuxtLink
-                        :title="`${userInfo.login}·${userInfo.name}`"
-                        :to="userInfo.url"
-                        class="text-lg leading-5 font-semibold text-mono"
-                        target="_blank"
-                    >
-                        {{ userInfo.name }}
-                    </NuxtLink>
-                    <span class="text-xs">@{{ userInfo.login }}</span>
-                </div>
-                <div class="flex flex-wrap justify-center gap-1 lg:gap-4.5 text-sm">
-                    <div class="flex gap-1.25 items-center">
-                        <Icon
-                            class="text-base-content/60"
-                            name="lucide:users"
-                        />
-                        <span class="text-base-content/60 font-medium">
-                            <span
-                                class="font-semibold text-info"
-                            >{{ userInfo.followers }}</span> followers
-                        </span>
-                    </div>
-                    <div class="flex gap-1.25 items-center">
-                        <Icon
-                            class="text-base-content/60"
-                            name="lucide:user-star"
-                        />
-                        <span class="text-base-content/60 font-medium">
-                            <span
-                                class="font-semibold text-success"
-                            >{{ userInfo.following }}</span> following
-                        </span>
-                    </div>
-                </div>
-            </div>
+  <div class="bg-center bg-cover bg-no-repeat bg-[url('/hero.png')] mb-5">
+    <div class="px-4">
+      <div
+        class="flex flex-col items-center gap-2 lg:gap-3.5 py-4 lg:pt-5 lg:pb-10"
+      >
+        <NuxtLink :title="namePlaceholder" :to="userInfo.url" target="_blank">
+          <img
+            :alt="namePlaceholder"
+            :src="userInfo.avatarUrl"
+            class="rounded-full size-28 shadow-[0_0_5px_1px_theme(color.green.500)] shrink-0"
+          />
+        </NuxtLink>
+        <div class="flex flex-col">
+          <p class="flex items-center gap-1.5">
+            <NuxtLink
+              :title="namePlaceholder"
+              :to="userInfo.url"
+              class="text-lg leading-5 font-semibold text-mono"
+              target="_blank"
+            >
+              {{ userInfo.name }}
+            </NuxtLink>
+            <span class="text-xs">@{{ userInfo.login }}</span>
+          </p>
+          <p class="text-center">
+            <span class="text-xs">{{ aliasName }}</span>
+          </p>
         </div>
+
+        <div class="flex flex-wrap justify-center gap-1 lg:gap-4.5 text-sm">
+          <div class="flex gap-1.25 items-center">
+            <Icon class="text-base-content/60" name="lucide:users" />
+            <span class="text-base-content/60 font-medium">
+              <span class="font-semibold text-info">{{
+                userInfo.followers
+              }}</span>
+              followers
+            </span>
+          </div>
+          <div class="flex gap-1.25 items-center">
+            <Icon class="text-base-content/60" name="lucide:user-star" />
+            <span class="text-base-content/60 font-medium">
+              <span class="font-semibold text-success">{{
+                userInfo.following
+              }}</span>
+              following
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import type { IGithubUserInfo } from '~/types'
+import type { IGithubUserInfo } from "~/types"
 
 defineOptions({
-    name: 'GithubUserInfo',
+  name: "GithubUserInfo",
 })
 
 const props = defineProps<{
-    userInfo: IGithubUserInfo
+  userInfo: IGithubUserInfo
+  aliasName?: string
 }>()
+
+const namePlaceholder = computed(() => {
+  const base = `${props.userInfo.login} · ${props.userInfo.name}`
+  return props.aliasName ? `${base} · ${props.aliasName}` : base
+})
 </script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,65 +1,70 @@
 <template>
-    <Tabs
-        v-model="tabValue"
-        class="flex flex-col lg:flex-row grow pt-(--header-height)"
+  <Tabs
+    v-model="tabValue"
+    class="flex flex-col lg:flex-row grow pt-(--header-height)"
+  >
+    <div
+      :class="
+        cn(
+          'fixed w-(--sidebar-width) lg:top-(--header-height) z-20',
+          'hidden lg:flex flex-col items-stretch shrink-0 group',
+          'py-3 lg:py-0'
+        )
+      "
     >
+      <TabsList class="flex grow shrink-0 w-full">
         <div
-            :class="cn(
-                'fixed w-(--sidebar-width) lg:top-(--header-height) z-20',
-                'hidden lg:flex flex-col items-stretch shrink-0 group',
-                'py-3 lg:py-0',
-            )"
+          class="overflow-y-auto gap-2.5 w-full flex flex-col items-center max-h-[calc(100dvh-10px))] lg:max-h-[calc(100dvh-70px))]"
         >
-            <TabsList class="flex grow shrink-0 w-full">
-                <div
-                    class="overflow-y-auto gap-2.5 w-full flex flex-col items-center max-h-[calc(100dvh-10px))] lg:max-h-[calc(100dvh-70px))]"
-                >
-                    <TabsTrigger
-                        v-for="member in members"
-                        :key="member"
-                        :value="member"
-                        as-child
-                        class="size-10 overflow-visible cursor-pointer"
-                    >
-                        <Avatar class="flex flex-col justify-center items-center">
-                            <AvatarImage
-                                :alt="member"
-                                :src="`https://github.com/${member}.png`"
-                                class="rounded-full"
-                            />
-                            <AvatarFallback>SN</AvatarFallback>
-                        </Avatar>
-                    </TabsTrigger>
-                </div>
-            </TabsList>
+          <TabsTrigger
+            v-for="member in members"
+            :key="member.username"
+            :value="member.username"
+            as-child
+            class="size-10 overflow-visible cursor-pointer"
+          >
+            <Avatar class="flex flex-col justify-center items-center">
+              <AvatarImage
+                :alt="member"
+                :src="`https://github.com/${member.username}.png`"
+                class="rounded-full"
+              />
+              <AvatarFallback>SN</AvatarFallback>
+            </Avatar>
+          </TabsTrigger>
         </div>
-        <div
-            class="flex grow rounded-lg border border-base-300 bg-white mx-5 lg:ms-(--sidebar-width) mb-5"
+      </TabsList>
+    </div>
+    <div
+      class="flex grow rounded-lg border border-base-300 bg-white mx-5 lg:ms-(--sidebar-width) mb-5"
+    >
+      <div
+        class="flex flex-col grow overflow-y-scroll lg:overflow-auto p-4 break-all"
+      >
+        <TabsContent
+          v-for="member in members"
+          :key="`tc-${member.username}`"
+          :value="member.username"
         >
-            <div
-                class="flex flex-col grow overflow-y-scroll lg:overflow-auto p-4 break-all"
-            >
-                <TabsContent
-                    v-for="member in members"
-                    :key="`tc-${member}`"
-                    :value="member"
-                >
-                    <GithubDashboard :data="VirtualMembers?.[tabValue]" />
-                </TabsContent>
-            </div>
-        </div>
-    </Tabs>
+          <GithubDashboard
+            :data="VirtualMembers?.[tabValue]"
+            :aliasName="member.aliasName"
+          />
+        </TabsContent>
+      </div>
+    </div>
+  </Tabs>
 </template>
 
 <script lang="ts" setup>
-import { members } from '~/constant/members'
+import { members } from "~/constant/members"
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
-import VirtualMembers from 'virtual-members'
-import { cn } from '~/lib/utils'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs'
-import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar'
-import GithubDashboard from '~/components/Github/GithubDashboard.vue'
+import VirtualMembers from "virtual-members"
+import { cn } from "~/lib/utils"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs"
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar"
+import GithubDashboard from "~/components/Github/GithubDashboard.vue"
 
-const tabValue = ref<string>(members?.[0] as string)
+const tabValue = ref<string>(members?.[0]?.username as string)
 </script>

aed250765f24edf0f73550ffe2a3a99bbb5a61d6
yuguaa <280722781@qq.com>
2026-01-30 10:41:47 +0800

    feat: 添加 worryzyy 的用户

--- app/constant/members.ts
+++ app/constant/members.ts
@@ -1,66 +1,74 @@
 type membersType = {
-    username: string
-    aliasName?: string
-}
+  username: string;
+  aliasName?: string;
+};
 export const members: membersType[] = [
-    { username: 'pkc918', aliasName: '肉丝哥' }, // 肉丝哥
-    { username: 'kovsu' }, // antdv 团队成员
-    { username: 'ilyaliao' }, // vueuse 团队成员
-    { username: 'Bao0630' }, // ant-design-x-vue 团队成员
-    { username: 'yuguaa', aliasName: '菠萝吹雪' }, // Element-Plus-X
-    { username: 'lonewolfyx' },
-    { username: 'ArthurDarkstone', aliasName: '高手哥' }, // 高手哥
-    { username: 'broBinChen', aliasName: '木木哥' }, // 木木哥
-    { username: 'NoiseFan', aliasName: '澎澎同学' },
-    { username: 'ffgenius', aliasName: '付伟斌' }, // 粉红色的回忆
-    { username: 'heyrian', aliasName: '日安' }, // 日安小姐姐
-    { username: 'LarryZhu-dev', aliasName: '朱永博' },
-    { username: 'doyuli', aliasName: '阿沥' },
-    { username: 'keeplearning66' },
-    { username: 'qianYuanJ' },
-    { username: 'snowbitx', aliasName: '社会哥' },
-    { username: 'TAYUN', aliasName: '云阁' },
-    { username: 'jsy-0526', aliasName: '阳哥' }, // 阳哥
-    { username: 'wan-kong' },
-    { username: 'penghangtao' },
-    { username: 'lbineau' },
-    { username: 'Chang-Chen' },
-]
+  { username: "pkc918", aliasName: "肉丝哥" }, // 肉丝哥
+  { username: "kovsu" }, // antdv 团队成员
+  { username: "ilyaliao" }, // vueuse 团队成员
+  { username: "Bao0630" }, // ant-design-x-vue 团队成员
+  { username: "yuguaa", aliasName: "菠萝吹雪" }, // Element-Plus-X
+  { username: "worryzyy", aliasName: "味蕾哥" }, // Element-Plus-X
+  { username: "lonewolfyx" },
+  { username: "ArthurDarkstone", aliasName: "高手哥" }, // 高手哥
+  { username: "broBinChen", aliasName: "木木哥" }, // 木木哥
+  { username: "NoiseFan", aliasName: "澎澎同学" },
+  { username: "ffgenius", aliasName: "付伟斌" }, // 粉红色的回忆
+  { username: "heyrian", aliasName: "日安" }, // 日安小姐姐
+  { username: "LarryZhu-dev", aliasName: "朱永博" },
+  { username: "doyuli", aliasName: "阿沥" },
+  { username: "keeplearning66" },
+  { username: "qianYuanJ" },
+  { username: "snowbitx", aliasName: "社会哥" },
+  { username: "TAYUN", aliasName: "云阁" },
+  { username: "jsy-0526", aliasName: "阳哥" }, // 阳哥
+  { username: "wan-kong" },
+  { username: "penghangtao" },
+  { username: "lbineau" },
+  { username: "Chang-Chen" },
+];
 
-export type OpenSourceTypes = 'team' | 'contributors'
+export type OpenSourceTypes = "team" | "contributors";
 type OpenSourceMembersType = {
-    [key: string]: {
-        [K in OpenSourceTypes]?: string[]
-    }
-}
+  [key: string]: {
+    [K in OpenSourceTypes]?: string[];
+  };
+};
 export const openSourceMembers: OpenSourceMembersType = {
-    'Vue': {
-        contributors: ['jsy-0526'],
-    },
-    'Vue Use': {
-        team: ['ilyaliao'],
-        contributors: ['pkc918', 'ArthurDarkstone', 'broBinChen', 'doyuli', 'snowbitx', 'jsy-0526'],
-    },
-    'MDN Docs Cn': {
-        contributors: ['lonewolfyx', 'pkc918'],
-    },
-    'ViteDevTools': {
-        contributors: ['lonewolfyx', 'pkc918'],
-    },
-    'Element Plus': {
-        contributors: ['keeplearning66', 'snowbitx'],
-    },
-    'Element Plus X': {
-        team: ['yuguaa'],
-        contributors: ['Chang-Chen'],
-    },
-    'Ant Design Vue': {
-        team: ['kovsu'],
-    },
-    'And Design X Vue': {
-        contributors: ['Bao0630'],
-    },
-    'Vitest Docs Cn': {
-        team: ['NoiseFan'],
-    },
-}
+  Vue: {
+    contributors: ["jsy-0526"],
+  },
+  "Vue Use": {
+    team: ["ilyaliao"],
+    contributors: [
+      "pkc918",
+      "ArthurDarkstone",
+      "broBinChen",
+      "doyuli",
+      "snowbitx",
+      "jsy-0526",
+    ],
+  },
+  "MDN Docs Cn": {
+    contributors: ["lonewolfyx", "pkc918"],
+  },
+  ViteDevTools: {
+    contributors: ["lonewolfyx", "pkc918"],
+  },
+  "Element Plus": {
+    contributors: ["keeplearning66", "snowbitx"],
+  },
+  "Element Plus X": {
+    team: ["yuguaa", "worryzyy"],
+    contributors: ["Chang-Chen"],
+  },
+  "Ant Design Vue": {
+    team: ["kovsu"],
+  },
+  "And Design X Vue": {
+    contributors: ["Bao0630"],
+  },
+  "Vitest Docs Cn": {
+    team: ["NoiseFan"],
+  },
+};
